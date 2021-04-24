c4791b1c1c540af806d9940d415f8a72b68fbcdd
admin <admin@DESKTOP-8L2IG92>
2021-04-25 05:44:15 +0900

    ex3no18

---
+++ ex3no18/src/ex3no18/ex3no18.java
@@ -0,0 +1,10 @@
+package ex3no18;
+
+public class ex3no18 { //107p
+	public static void main(String[] args) {
+		double pi = 3.141592;
+		double shortPi = Math.round(pi * 1000) / 1000.0;
+		System.out.println(shortPi);
+	}
+
+}
